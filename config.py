81809704a6449b1c316faeedb24e1d793df39f46
Jaycoap <70280331+jaycoap@users.noreply.github.com>
2023-03-09 10:34:54 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 db = {
     'user'     : 'root',
-    'password' : '625wjswod',
-    'host'     : 'localhost',
-    'port'     : 3306,
-    'database' : 'miniter'
+    'password' : '',
+    'host'     : '',
+    'port'     : ,
+    'database' : ''
 }
 DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
 test_config={
